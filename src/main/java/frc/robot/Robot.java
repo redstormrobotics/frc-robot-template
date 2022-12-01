5aa46d2b4effc1dd17519afae5de8c3822d6737b
FRC Brown <frc-brown@redstormrobotics.com>
2022-11-30 21:12:23 -0500

    Added Skeletons of Modes.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,7 +1,39 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
+/*============================================================
+/ Red Storm Robotics 2022/23
+/=============================================================
+/ ROBOT
+/
+/ This is where all objects are initialized for the entire
+/ System. 
+/=============================================================
+/ Hardware:
+/
+/ See configuration of entire system
+/=============================================================
+/ 3rd Party Dependencies:
+/
+/=============================================================
+/ Permission is hereby granted, free of charge, to any 
+/ person obtaining a copy of this software and associated 
+/ documentation files (the "Software"), to deal in the 
+/ Software without restriction, including without limitation 
+/ the rights to use, copy, modify, merge, publish, distribute, 
+/ sublicense, and/or sell copies of the Software, and to 
+/ permit persons to whom the Software is furnished to do so, 
+/ subject to the following conditions:
+/ 
+/ The above copyright notice and this permission notice shall 
+/ be included in all copies or substantial portions of the Software.
+/
+/ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY 
+/ KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+/ WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+/ PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
+/ OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR 
+/ OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+/ OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+/ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+/=================================================================*/
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -9,27 +41,56 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * The Robot is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
+
+	// User-Side Controls
 	private static final String kDefaultAuto = "Default";
 	private static final String kCustomAuto = "My Auto";
 	private String m_autoSelected;
 	private final SendableChooser<String> m_chooser = new SendableChooser<>();
+	private Gamepad gp1;
+	private Gamepad gp2;
+
+	// Robot Hardware Attached:
+	private int counter;
+	private Config config;
+
+	// Operating Modes
+	private ModeAuton modeAuton;
+	private ModeTeleOp modeTeleOp;
+	private ModeDisabled modeDisabled;
+	private ModeSimulation modeSimulation;
+	private ModeTest modeTest;
 
 	/**
 	 * This function is run when the robot is first started up and should be used for any
 	 * initialization code.
 	 */
 	@Override
-	public void robotInit() {
+	public void robotInit() 
+	{
+
+		// Initialize User-Side Controls
 		m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
 		m_chooser.addOption("My Auto", kCustomAuto);
 		SmartDashboard.putData("Auto choices", m_chooser);
 		System.out.println("robotInit() complete");
+		gp1 = new Gamepad(0);
+		gp2 = new Gamepad(1);
+
+		// Initialization of all Hardware
+		counter = 0;
+		config = new Config(0);
+		modeAuton = new ModeAuton(config);
+		modeTeleOp = new ModeTeleOp(config);
+		modeSimulation = new ModeSimulation(config);
+		modeTest = new ModeTest(config);
+		modeDisabled = new ModeDisabled(config);
 	}
 
 	/**
@@ -40,68 +101,88 @@ public class Robot extends TimedRobot {
 	 * SmartDashboard integrated updating.
 	 */
 	@Override
-	public void robotPeriodic() {}
+	public void robotPeriodic() 
+	{
+		if ((++counter % 10) == 0)
+		{
+			SmartDashboard.putNumber("Left", gp1.getLeftY());
+			SmartDashboard.putNumber("Right", gp1.getRightY());
+			SmartDashboard.putNumber("Counter", counter);
+		}
+	}
 
 	/**
 	 * This autonomous (along with the chooser code above) shows how to select between different
 	 * autonomous modes using the dashboard. The sendable chooser code works with the Java
-	 * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-	 * uncomment the getString line to get the auto name from the text box below the Gyro
-	 *
+	 * SmartDashboard.
+	 * 
 	 * <p>You can add additional auto modes by adding additional comparisons to the switch structure
 	 * below with additional strings. If using the SendableChooser make sure to add them to the
 	 * chooser code above as well.
 	 */
 	@Override
-	public void autonomousInit() {
+	public void autonomousInit() 
+	{
 		m_autoSelected = m_chooser.getSelected();
 		// m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
 		System.out.println("Auto selected: " + m_autoSelected);
+		modeAuton.Initialize();
+		modeAuton.selectAuton(m_autoSelected);
 	}
 
 	/** This function is called periodically during autonomous. */
 	@Override
-	public void autonomousPeriodic() {
-		switch (m_autoSelected) {
-		  case kCustomAuto:
-			// Put custom auto code here
-			break;
-		  case kDefaultAuto:
-		  default:
-			// Put default auto code here
-			break;
-		}
+	public void autonomousPeriodic() 
+	{
+		modeAuton.Periodic();
 	}
 
-	/** This function is called once when teleop is enabled. */
 	@Override
-	public void teleopInit() {}
+	public void teleopInit() 
+	{
+		modeTeleOp.Initialize();
+	}
 
-	/** This function is called periodically during operator control. */
 	@Override
-	public void teleopPeriodic() {}
+	public void teleopPeriodic() 
+	{
+		modeTeleOp.Periodic();
+	}
 
-	/** This function is called once when the robot is disabled. */
 	@Override
-	public void disabledInit() {}
+	public void disabledInit() 
+	{
+		modeDisabled.Initialize();
+	}
 
-	/** This function is called periodically when disabled. */
 	@Override
-	public void disabledPeriodic() {}
+	public void disabledPeriodic() 
+	{
+		modeDisabled.Periodic();
+	}
 
-	/** This function is called once when test mode is enabled. */
 	@Override
-	public void testInit() {}
+	public void testInit() 
+	{
+		modeTest.Initialize();
+	}
 
-	/** This function is called periodically during test mode. */
 	@Override
-	public void testPeriodic() {}
+	public void testPeriodic() 
+	{
+		modeTest.Periodic();
+	}
 
-	/** This function is called once when the robot is first started up. */
 	@Override
-	public void simulationInit() {}
+	public void simulationInit() 
+	{
+		modeSimulation.Initialize();
+	}
 
-	/** This function is called periodically whilst in simulation. */
 	@Override
-	public void simulationPeriodic() {}
+	public void simulationPeriodic()
+	{		
+		modeSimulation.Periodic();
+	}
+
 }
