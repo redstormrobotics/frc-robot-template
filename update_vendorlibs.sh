b680961ef7f56fe1392a8d567fceeacc54569982
Andy Tomaszewski <zueski@gmail.com>
2023-12-12 16:10:49 -0500

    guessed the new REV url

--- update_vendorlibs.sh
+++ update_vendorlibs.sh
@@ -14,7 +14,7 @@ gradle vendordep --url=https://maven.photonvision.org/repository/internal/org/ph
 
 # from https://docs.revrobotics.com/sparkmax/software-resources/spark-max-api-information#online-installation
 echo =================== updating Rev libs ===================
-gradle vendordep --url=https://software-metadata.revrobotics.com/REVLib-2023.json
+gradle vendordep --url=https://software-metadata.revrobotics.com/REVLib-2024.json
 
 # from https://www.playingwithfusion.com/docview.php?docid=1205
 echo =================== updating PlayingWithFusion libs ===================
